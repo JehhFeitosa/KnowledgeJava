09c75f96d16fb861f844ea679d2a767d97fec58c
jessica.feitosa <jessica.feitosa@inmetrics.com.br>
2020-11-29 23:06:37 -0300

    Adicionando informações no README

--- src/main/java/com/jessica/feitosa/aulaspoo/aula02/Caneta.java
+++ src/main/java/com/jessica/feitosa/aulaspoo/aula02/Caneta.java
@@ -9,8 +9,11 @@ public class Caneta {
 	boolean tampada;
 	
 	void status() {
-		System.out.println("uma  caneta "+ this.cor);
-		System.out.println("está tampada? "+ this.tampada);
+		System.out.println("Modelo: " + this.modelo);
+		System.out.println("Uma  caneta "+ this.cor);
+		System.out.println("Ponta: " + this.ponta);
+		System.out.println("Carga: " + this.carga);
+		System.out.println("Está tampada? "+ this.tampada);
 	}
 
 	void rabiscar() {
