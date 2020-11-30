44d720d6eceea3b7b93ab0ef589f33ea8ee12517
jessica.feitosa <jessica.feitosa@inmetrics.com.br>
2020-11-30 02:42:42 -0300

    Aula 4 POO AJUSTES

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # KnowledgeJava
 
+#Exercícios das aulas de Java Básico com Loiane Groner.
 
-Exercícios das aulas de Java Básico com Loiane Groner
-
-Exercícios das aulas de Poo com o Mestre Guanabara
+#Exercícios das aulas de Poo com o Mestre Guanabara.
--- src/main/java/com/jessica/feitosa/aulaspoo/aula04/Aula04.java
+++ src/main/java/com/jessica/feitosa/aulaspoo/aula04/Aula04.java
@@ -10,13 +10,6 @@ public class Aula04 {
 		Caneta c2 = new Caneta("BIC", "Rosa", 0.7f);
 		c1.status();
 		
-//		c1.setModelo("BIC");
-//		//c1.modelo = "BIC";
-//		
-//		c1.setPonta(0.5f);
-//		
-//		
-//		System.out.println("Tenho uma caneta " + c1.getModelo() + " de ponta " + c1.getPonta());
 
 	}
 
