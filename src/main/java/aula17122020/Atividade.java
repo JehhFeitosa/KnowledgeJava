a1f46d519a01a7f8bb8e6c5d4bc4ed81b8be2a9c
jessica.feitosa <jessica.feitosa@inmetrics.com.br>
2020-12-18 09:21:25 -0300

    Aula algoritmo

---
+++ src/main/java/aula17122020/Atividade.java
@@ -0,0 +1,47 @@
+package aula17122020;
+
+import java.util.Scanner;
+
+public class Atividade {
+
+	public static void main(String[] args) {
+		
+		
+		int alt, b, a;
+		
+		Scanner s = new Scanner (System.in);
+		
+		System.out.println("Digite a altura de um retângulo: ");
+		 a= s.nextInt();
+		
+		
+		
+	
+	 
+	 
+		
+		
+	}
+	
+//	int	num1, num2;
+//	 Scanner s = new Scanner (System.in);
+//	 
+//	 System.out.println("Digite um número");
+//	 num1= s.nextInt();
+//	 num2= num1 - 1;
+//	 
+//	 System.out.println("O número antecessor de: "+ num1 + " é : " + num2);
+		
+
+//		int a, b, c;
+//
+//		a= 10;
+//		b= 20;
+//		c= a;
+//		a= b;
+//		b= c;
+//		
+//		System.out.println("A = "+ a + " B = " + b);
+//	}
+
+}
