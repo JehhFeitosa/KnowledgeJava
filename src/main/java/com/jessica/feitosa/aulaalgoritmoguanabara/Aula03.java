29644e5cf63201964558ad1f26736118ebecbe30
jessica.feitosa <jessica.feitosa@inmetrics.com.br>
2020-12-20 14:35:45 -0300

    Aula algoritmo 03 guanabara

---
+++ src/main/java/com/jessica/feitosa/aulaalgoritmoguanabara/Aula03.java
@@ -0,0 +1,25 @@
+package com.jessica.feitosa.aulaalgoritmoguanabara;
+
+import java.util.Scanner;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+
+		int n1, n2, soma;
+		Scanner s = new Scanner(System.in);
+
+		System.out.println("Digite um número ");
+		n1 = s.nextInt();
+
+		System.out.println("Digite outro número ");
+		n2 = s.nextInt();
+		
+		soma= n1+n2;
+		
+		System.out.println("A soma dos número é: " + soma);
+
+		
+	}
+
+}
