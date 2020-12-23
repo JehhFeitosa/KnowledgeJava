bd9ad77277df0790c8ea967077bc8fb60958e71e
jessica.feitosa <jessica.feitosa@inmetrics.com.br>
2020-12-23 11:15:47 -0300

    Exercicios 01 e 02 das aulas de algoritmos guanabara

---
+++ src/main/java/com/jessica/feitosa/aulaalgoritmoguanabara/Exer01.java
@@ -0,0 +1,12 @@
+package com.jessica.feitosa.aulaalgoritmoguanabara;
+
+//Faça um Hello World!
+public class Exer01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Olá, mundo!");
+
+	}
+
+}
